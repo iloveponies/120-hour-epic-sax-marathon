f598cca8f6c15ac0d893fe37fd83d669ab8a68f9
Ilmari Vacklin <ilmari.vacklin@eficode.com>
2012-04-29 23:13:44 +0300

    Syntax cleanup

--- evaluate-examples.pl
+++ evaluate-examples.pl
@@ -1,46 +1,55 @@
 #!/usr/bin/env perl
 
+use v5.14;
 use strict;
 use warnings;
 use utf8::all;
-use v5.14;
+use autodie;
+
+# Main
+
+parse();
+
+# Subroutines
+
+sub parse {
+    my @blocks;
+    my $current_block = "";
+    my $state = "out";
+
+    while (my $line = <>) {
+        if ($state eq "out") {
+            if ($line eq "~~~ {.clojure}\n") {
+                $state = "in";
+                push @blocks, $current_block . $line;
+                $current_block = "";
+            }
+            else {
+                $current_block .= $line;
+            }
+        }
+        elsif ($state eq "in") {
+            if ($line eq "~~~\n") {
+                $state = "out";
+                push @blocks, evaluate($current_block);
+                $current_block = $line;
+            }
+            elsif ($line eq "~~~ {.clojure}\n") {
+                die "syntax error;"
+            }
+            else {
+                $current_block .= $line;
+            }
+        }
+    }
+
+    push @blocks, $current_block;
+
+    say join "", @blocks;
+}
 
 sub evaluate {
     my ($block) = @_;
 
-    return "EVALUATED LOL";
+    return "EVALUATED LOL\n";
 }
-
-my @blocks;
-my $current_block = "";
-my $state = "out";
-
-while (chomp (my $line = <>)) {
-    if ($state eq "out") {
-        if ($line eq '~~~ {.clojure}') {
-            $state = "in";
-            push @blocks, $current_block . "\n" . $line;
-            $current_block = "";
-        }
-        else {
-            $current_block .= "\n" . $line;
-        }
-    }
-    elsif ($state eq "in") {
-        if ($line eq '~~~') {
-            $state = "out";
-            push @blocks, evaluate($current_block);
-            $current_block = $line;
-        }
-        elsif ($line eq '~~~ {.clojure}') {
-            die "syntax error;"
-        }
-        else {
-            $current_block .= "\n" . $line;
-        }
-    }
-}
-
-push @blocks, $current_block;
-
-say join "\n", @blocks;
