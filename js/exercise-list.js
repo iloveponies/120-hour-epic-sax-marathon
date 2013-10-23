0566511c74398b44d63db4d26b0bd4919126bee9
Jani Rahkola <jani.rahkola@iki.fi>
2013-10-24 00:40:35 +0300

    using bootstrap 3
    moved the exercise list into a second column

--- js/exercise-list.js
+++ js/exercise-list.js
@@ -1,11 +1,13 @@
 jQuery(function($) {
-    var sidebar = $('<ul>', { 'class': 'sidebar-exercise-list nav nav-list' });
+    var exercise_list = $('#exercise-list');
+    //var sidebar = $('<ul>', { 'class': 'sidebar-exercise-list nav nav-list' });
     $('.alert-success').each(function(i, elem) {
         $(elem).attr('id', 'exercise-' + i);
 
         var exerciseName = $(elem).find('h3').text();
         var link = $('<a>', { href: '#exercise-' + i }).text(exerciseName);
-        sidebar.append($('<li>').append(link));
+        //sidebar.append($('<li>').append(link));
+        exercise_list.append($('<li>').append(link));
     });
-    $('body').prepend(sidebar);
+    //$('body').prepend(sidebar);
 });
